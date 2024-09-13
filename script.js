d84f6292cbd63a3cc35460f01d8aaefdfe41b405
Pradnya Chandrakant Bhosale <67332759+PRADNYA199@users.noreply.github.com>
2024-09-13 10:11:12 -0700

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
-//your JS code here. If required.
-
 document.getElementById("incrementBtn").addEventListener("click", function() {
-    
     let counterElement = document.getElementById("counter");
-    
-    
     let currentValue = parseInt(counterElement.textContent);
-    
-   
     alert("Current value before increment: " + currentValue);
-    
-    
     counterElement.textContent = currentValue + 1;
 });
