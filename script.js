60b3432fe9349df9d89d3f243a75870d223a85ca
Pradnya Chandrakant Bhosale <67332759+PRADNYA199@users.noreply.github.com>
2024-09-13 10:01:21 -0700

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 //your JS code here. If required.
-let count=0;
-<button onclick="myFunction()">Increment</button>
-<p id="counter"></p>
+
 function myFunction() {
   document.getElementById("counter").innerHTML = count++;
 }
